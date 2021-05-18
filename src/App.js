fcd9d939ba89f1a1330593c21d75704155e5a6d6
Roman Hatz SRF <roman.hatz@srf.ch>
2021-05-18 09:58:20 +0200

    step 1

---
+++ src/App.js
@@ -0,0 +1,31 @@
+ 
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Step 1: Meteo Map Component hierarchy</h1>
+      <ul>
+        <li>Header</li>
+        <ul>
+          <li>Title</li>
+          <li>Navigation</li>
+        </ul>
+
+        <li>Content</li>
+        <ul>
+          <li>Map</li>
+          <li>Map Controls  </li>
+        </ul>
+
+        <li>Footer</li>
+        <ul>
+          <li>Sources</li>
+        </ul>
+      </ul>
+        
+    </>
+  );
+}
+
+export default App;
